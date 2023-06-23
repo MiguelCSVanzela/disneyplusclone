fe733142faa14ba04974c7b8be70fcf3b94ec34c
Miguel CS Vanzela <miguelclaudiodasilvavanzela@gmail.com>
2023-06-23 10:58:11 -0300

    Ajustes mobile

--- source/main.js
+++ source/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < heroHeight) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    // Selção de Atrações
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (button) {
             const abaAlvo = button.target.dataset.tabButton;
@@ -25,5 +41,29 @@ document.addEventListener('DOMContentLoaded', function () {
         for (let i = 0; i < tabsContainer.length; i++) {
             tabsContainer[i].classList.remove('shows__list--is-active');
         }
+    }
+
+    //Seção Faq
+    for (let i = 0; questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta());
+    }
+
+
+    function abreOuFechaResposta(elemento) {
+        const classe = 'faq__questions__item--is-open';
+        const elementoPai = elemento.target.parentNode;
+
+        elementoPai.classList.toggle(classe);
+    }
+
+    // Header
+    function ocultaElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+
+    function exibeElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
     }
 })
